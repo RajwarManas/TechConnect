6c08fc2db60b9d3101ddd813468c436e4c7811bd
RajwarManas <manasrajwar.28@gmail.com>
2026-08-12 11:18:32 +0530

    Configured settings for Neon integration

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -97,6 +97,9 @@ DATABASES = {
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
         "PORT": config("DB_PORT"),
+        "OPTIONS": {
+            "sslmode": config("DB_SSLMODE", default="disable"),
+        },
     }
 }
 
